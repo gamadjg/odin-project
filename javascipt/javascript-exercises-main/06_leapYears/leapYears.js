7194480ed1c99ecc8b186bca0429ea212430a056
varidjg <david@variusit.org>
2021-12-15 04:37:06 -0800

    JS exercise 6 complete.

--- javascipt/javascript-exercises-main/06_leapYears/leapYears.js
+++ javascipt/javascript-exercises-main/06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(yr) {
+    
+    if (yr%4 === 0){
+        if (!(yr%400 === 0) && (yr%100 === 0)){
+            return false;
+        };
+        return true;
+    }
+    return false;
+
+//divisible by four or 400
+//nless also divisible by 100
 
 };
 
