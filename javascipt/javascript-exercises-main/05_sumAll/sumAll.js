624b3740ca5b036fbcc36917a5f1ccfd044bbaa9
varidjg <david@variusit.org>
2021-12-15 04:03:29 -0800

    JS exercise 5 complete.

--- javascipt/javascript-exercises-main/05_sumAll/sumAll.js
+++ javascipt/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let tempVal = 0;
+    let largerVal = 0;
+    let smallerVal = 0;
+    
+    // If either x or y is a string, even a number string
+    if ((typeof x == 'string') || (typeof y == 'string')){
+        return 'ERROR';
+    }
+    // If either value is not a number
+    else if ((isNaN(x)) || (isNaN(y))){
+        return 'ERROR';
+    }
+    // If either value is a negative number
+    else if ((x <=0) || (y<=0)){
+        return 'ERROR';
+    }
+    // All values g2g
+    else {
+        if (x > y){
+            largerVal = x;
+            smallerVal = y;
+        }
+        else {
+            largerVal = y;
+            smallerVal = x;
+        };
 
+        for (i=smallerVal; i<=largerVal; i++){
+            tempVal += i;
+        }
+        return tempVal;
+    };
 };
 
 // Do not edit below this line
