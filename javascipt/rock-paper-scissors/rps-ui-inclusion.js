ae06096628b24d12c05561b337b3e42bf8918c96
GamaDJG <gamadavid36@gmail.com>
2022-01-06 16:39:38 -0700

    btn eventlistener & npc selection done

--- javascipt/rock-paper-scissors/rps-ui-inclusion.js
+++ javascipt/rock-paper-scissors/rps-ui-inclusion.js
@@ -8,3 +8,42 @@
  * After 5 rounds, display who won best of 5. 
  * Insert "play again?" button
  */
+//let roundCounter = 0;
+//let BtnSel = playerBtnSel;
+
+function npcSelection(){
+  const oneToThreeRnd = Math.floor(Math.random() * 3 + 1);
+
+  if (oneToThreeRnd === 1){
+      return 'rock';
+  }
+  else if (oneToThreeRnd === 2){
+      return 'paper';
+  }
+  else{
+      return 'scissors'
+  }
+}
+
+function compareSelections(){
+
+}
+
+function displayResults(){
+
+}
+
+function playRound(playerSel){
+  //console.log(playerSel);
+  const npcSel = npcSelection();
+  //console.log(npcSel);
+
+  const roundWinner = compareSelections(playerSel, npcSel);
+  displayResults();
+}
+
+const buttons = document.querySelectorAll(".rock, .paper, .scissors");
+buttons.forEach(btn => btn.addEventListener('click', () =>{
+  //console.log(btn.className);
+  playRound(btn.className);
+}));
