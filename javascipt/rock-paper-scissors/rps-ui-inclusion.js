b4aecbb3bcc42bc0b3ae6fb9d106da8f899b699b
GamaDJG <gamadavid36@gmail.com>
2022-01-06 13:54:04 -0700

    rps pseudocode steps

--- javascipt/rock-paper-scissors/rps-ui-inclusion.js
+++ javascipt/rock-paper-scissors/rps-ui-inclusion.js
@@ -1,5 +1,10 @@
 /*
-
-
-
-*/
+ * Add on click event listener to each button
+ * When clicked, run the function playRound(playerSelection Attr)
+ * Call the function npcSelection(), return results
+ * Compare player and npc selection
+ * Update round counter
+ * Display round winner
+ * After 5 rounds, display who won best of 5. 
+ * Insert "play again?" button
+ */
