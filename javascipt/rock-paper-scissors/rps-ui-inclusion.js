2a77bb1aaacce43b860905186718012f7acd55ac
GamaDJG <gamadavid36@gmail.com>
2022-01-06 23:47:48 -0700

    rps dom refactor + new game feature complete

--- javascipt/rock-paper-scissors/rps-ui-inclusion.js
+++ javascipt/rock-paper-scissors/rps-ui-inclusion.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Add on click event listener to each button
  * When clicked, run the function playRound(playerSelection Attr)
  * Call the function npcSelection(), return results
@@ -8,63 +8,163 @@
  * After 5 rounds, display who won best of 5. 
  * Insert "play again?" button
  */
-//let roundCounter = 0;
-//let BtnSel = playerBtnSel;
+ var roundCounter = 1;
+ var maxGames = 5;
+ var playerTotalWins = 0;
+ var computerTotalWins = 0;
+ var totalTies = 0;
+ var playGame = true;
 
-function npcSelection(){
+function findCompSelection(){
   const oneToThreeRnd = Math.floor(Math.random() * 3 + 1);
 
   if (oneToThreeRnd === 1){
       return 'rock';
-  }
-  else if (oneToThreeRnd === 2){
+
+  }else if (oneToThreeRnd === 2){
       return 'paper';
-  }
-  else{
+  
+  }else{
       return 'scissors'
   }
-}
+}  
 
 function compareSelections(playerSelection, computerSelection){
   if ((playerSelection === 'rock') && (computerSelection === 'paper')){
-    return 'computer win';
-  }
-  else if ((playerSelection === 'paper') && (computerSelection === 'scissors')){
-    return 'computer win';
-  }
-  else if ((playerSelection === 'scissors') && (computerSelection === 'rock')){
-    return 'computer win';
-  }
-  else if ((playerSelection === 'rock') && (computerSelection === 'scissors')){
-    return 'player win';
-  }
-  else if ((playerSelection === 'paper') && (computerSelection === 'rock')){
-    return 'player win';
-  }
-  else if ((playerSelection === 'scissors') && (computerSelection === 'paper')){
-    return 'player win';
-  }
-  else{
-    // playerSelection and computerSelection are equal
+    computerTotalWins+=1;
+    return 'NPC won the round';
+  
+  }else if ((playerSelection === 'paper') && (computerSelection === 'scissors')){
+    computerTotalWins+=1;
+    return 'NPC won the round';
+  
+  }else if ((playerSelection === 'scissors') && (computerSelection === 'rock')){
+    computerTotalWins+=1;
+    return 'NPC won the round';
+  
+  }else if ((playerSelection === 'rock') && (computerSelection === 'scissors')){
+    playerTotalWins+=1;
+    return 'You won the round';
+  
+  }else if ((playerSelection === 'paper') && (computerSelection === 'rock')){
+    playerTotalWins+=1;
+    return 'You won the round';
+  
+  }else if ((playerSelection === 'scissors') && (computerSelection === 'paper')){
+    playerTotalWins+=1;
+    return 'You won the round';
+  
+  }else{
+    totalTies+=1;
     return 'TIE!';
   }
 }
 
-function displayResults(roundWinner){
-  console.log(roundWinner)
+function endGame(){
+  const roundWinnerNode = document.querySelector('.whoWon');
+  const totalPlayerLosses = maxGames - totalTies - playerTotalWins;
+  if (playerTotalWins > computerTotalWins){
+    roundWinnerNode.textContent = `You won the game with ${playerTotalWins} wins!`
+  
+  }else if (playerTotalWins < computerTotalWins){
+    roundWinnerNode.textContent = `You lost the game with ${totalPlayerLosses} losses...`;
+  
+  }else{
+    roundWinnerNode.textContent = 'You and the NPC tied.'
+  }
+  return
 }
 
-function playRound(playerSel){
-  //console.log(playerSel);
-  const npcSel = npcSelection();
-  //console.log(npcSel);
+function displayResults(roundWinner, playerSelection, computerSelection){
+  const whatRound = document.querySelector('.result-counter');
+  const playerResultsNode = document.querySelector('.player-selection');
+  const computerResultsNode = document.querySelector('.npc-selection');
+  const roundWinnerNode = document.querySelector('.whoWon');
 
-  const roundWinner = compareSelections(playerSel, npcSel);
-  displayResults(roundWinner);
+  whatRound.textContent = roundCounter;
+
+  if (roundCounter == maxGames){
+    //const totalPlayerLosses = maxGames - totalTies - playerTotalWins;
+    if (playerTotalWins > computerTotalWins){
+      roundWinnerNode.textContent = `You won the game!`
+    
+    }else if (playerTotalWins < computerTotalWins){
+      roundWinnerNode.textContent = `You lost the game`;
+    
+    }else{
+      roundWinnerNode.textContent = 'You and the NPC tied.'
+    }
+    playGame = false;
+    setupNewGame();
+
+  }else if (roundCounter < maxGames) {
+    playerResultsNode.textContent = playerSelection;
+    computerResultsNode.textContent = computerSelection;
+    roundWinnerNode.textContent = roundWinner;
+    whatRound.textContent = roundCounter;
+    roundCounter+=1;
+  }else{
+    return;
+  }
 }
+
+function playRound(playerSelection){
+  if (playGame){
+    const computerSelection = findCompSelection();
+    console.log(`comp selection: ${computerSelection}`);
+    console.log(`player selection: ${playerSelection}`);
+
+    const roundWinner = compareSelections(playerSelection, computerSelection);
+    displayResults(roundWinner, playerSelection, computerSelection);
+  }else{
+
+  }
+}
+
+console.log(`round count: ${roundCounter}`);
+console.log(`maxGames: ${maxGames}`);
+console.log(`player wins: ${playerTotalWins}`);
+console.log(`computer wins: ${computerTotalWins}`);
+console.log(`total ties: ${totalTies}`);
+console.log(`play?: ${playGame}`);
 
 const buttons = document.querySelectorAll(".rock, .paper, .scissors");
 buttons.forEach(btn => btn.addEventListener('click', () =>{
-  //console.log(btn.className);
   playRound(btn.className);
+
 }));
+/**
+ * -------------------------------FEATURES-------------------------------
+ * Use the maxGames class to set how many games you would like to play
+ * 
+ * X - Add a play again button that resets all the counters
+ * 
+ * Display current player and npc wins
+ */
+
+ function setupNewGame(){
+  const bodyContainer = document.querySelector('.body-c');
+
+  const newGameContainer = document.createElement('div');
+  newGameContainer.classList.add('.ng-c');
+  bodyContainer.appendChild(newGameContainer);
+
+
+  const ngBtn = document.createElement('button');
+  ngBtn.textContent = 'Play again?';
+  ngBtn.classList.add('.ngBtnClass')
+  ngBtn.style.color = 'forestgreen';
+  ngBtn.style.fontSize = '20px';
+  ngBtn.style.padding = '10px 20px 10px 20px';
+  newGameContainer.appendChild(ngBtn);
+
+  ngBtn.addEventListener('click', () =>{
+    location.reload();
+  })
+  /*
+  newGameButton.addEventListener('click', ()=>{
+    console.log('test');
+    location.reload();
+  });
+  */
+}
