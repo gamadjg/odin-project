840462531735fccc09053f5a1ea4bfc0fb7fe7f2
gamadjg <gamadavid36@gmail.com>
2022-01-23 11:22:31 -0800

    Created prettierrc for project

--- javascipt/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascipt/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+	fibArray = [0];
+	let fib = 0;
+	for (let i = 1; i < a; i++) {
+		if ((i = 1)) {
+			fib += 1;
+			fibArray.push(fib);
+		} else {
+			console.log(fibArray.at(-1));
+			console.log(fibArray.at(-2));
+			fib = fibArray.at(-1) + fibArray.at(-2);
+			fibArray.push(fib);
+			console.log(fib);
+		}
+	}
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
