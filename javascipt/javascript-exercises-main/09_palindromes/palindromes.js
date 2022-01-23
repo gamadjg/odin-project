367c8bb369d1e3e524c55a37cd7972945cd709f0
gamadjg <gamadavid36@gmail.com>
2022-01-23 04:25:55 -0800

    Completed ex 09_palindromes

--- javascipt/javascript-exercises-main/09_palindromes/palindromes.js
+++ javascipt/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (atr) {
+	str = atr.toLowerCase().replace(/[^a-z]/g, "");
+	arr = Array.from(str);
+	nstr = arr
+		.reverse()
+		.toString()
+		.replace(/[^a-zA-Z]/g, "");
+	return str === nstr ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
