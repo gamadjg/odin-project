1b23d1275318dda1f01ff42898e05ee40c46d94c
gamadjg <gamadavid36@gmail.com>
2022-01-23 18:08:38 -0800

    ex11, correct val pull, pending array return

--- javascipt/javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ javascipt/javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+	let ret_arr = [];
+	arr.forEach((element) => {
+		//console.log(element.title);
+	});
 };
 
+const books = [
+	{
+		title: "Book",
+		author: "Name",
+	},
+	{
+		title: "Book2",
+		author: "Name2",
+	},
+];
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
