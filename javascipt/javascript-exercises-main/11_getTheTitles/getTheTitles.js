f668ec9dfbebadeda107333e448d137b0448a2df
gamadjg <gamadavid36@gmail.com>
2022-01-23 18:11:54 -0800

    ex11 complete

--- javascipt/javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ javascipt/javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,20 +1,10 @@
 const getTheTitles = function (arr) {
 	let ret_arr = [];
 	arr.forEach((element) => {
-		//console.log(element.title);
+		ret_arr.push(element.title);
 	});
+	return ret_arr;
 };
 
-const books = [
-	{
-		title: "Book",
-		author: "Name",
-	},
-	{
-		title: "Book2",
-		author: "Name2",
-	},
-];
-getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
