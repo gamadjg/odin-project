75367d1cddb97f79e1c9ee3974afdb05ffefa4bd
varidjg <david@variusit.org>
2022-01-30 17:08:12 -0800

    ex12 complete

--- javascipt/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascipt/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	let today = new Date();
+	let year = today.getFullYear();
 
+	let lifeLength = 0;
+	let longestLife = 0;
+	let oldestPerson = {};
+	people.forEach((person) => {
+		if ("yearOfDeath" in person) {
+			lifeLength = person.yearOfDeath - person.yearOfBirth;
+		} else {
+			lifeLength = year - person.yearOfBirth;
+		}
+		if (lifeLength > longestLife) {
+			longestLife = lifeLength;
+			oldestPerson = person;
+		}
+	});
+	return oldestPerson;
 };
 
 // Do not edit below this line
