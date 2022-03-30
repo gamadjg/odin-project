66955e8a26cc37d22f5a7b776c358404776a1960
gamadjg <gamadavid36@gmail.com>
2022-03-30 02:47:49 -0700

    hooks review, useState & useEffect complete

--- react-js/react-playground/src/components/App.js
+++ react-js/react-playground/src/components/App.js
@@ -3,6 +3,7 @@ import SideNav from "./side-nav";
 import CC_LIFECYCLE_TIMERS from "./CC_LIFECYCLE_TIMERS";
 import CC_LIFECYCLE_CLICKS from "./CC_LIFECYCLE_CLICKS";
 import CycleString from "./cycle-string";
+import HooksReview from "./fc-hooks-review";
 
 //import FC_INCREMENTAL_COUNT from "./FC_INCREMENTAL_COUNT";
 //import FC_ONCLICK_COLORING from "./FC_ONCLICK_COLORING";
@@ -18,6 +19,7 @@ function App() {
 				<div className="mainBodyContainer">
 					<CycleString />
 					<CC_LIFECYCLE_TIMERS />
+					<HooksReview />
 					<CC_LIFECYCLE_CLICKS />
 				</div>
 			</div>
