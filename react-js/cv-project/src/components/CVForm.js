3c4cf5cd705130d50221c11a4abb98407628f92d
gamadjg <gamadavid36@gmail.com>
2022-04-05 00:03:29 -0700

    Refactored, 1 JSX return for all fields

--- react-js/cv-project/src/components/CVForm.js
+++ react-js/cv-project/src/components/CVForm.js
@@ -13,14 +13,35 @@ const CVForm = ({ setFirstName, setLastName }) => {
 		}
 	}, [formSubmit]);
 
-	const fnChange = (event) => {
-		setFirstName(event.target.value);
-		setLocalFirstName(event.target.value);
+	const propField = (formField, event) => {
+		console.log("test");
+		if (formField === "firstName") {
+			setFirstName(event.target.value);
+		}
 	};
 
-	const lnChange = (event) => {
-		setLastName(event.target.value);
-		setLocalLastName(event.target.value);
+	const getFieldData = (renderType) => {
+		if (renderType === "firstName") {
+			return ["firstName", "First Name", "text"];
+		}
+	};
+
+	const renderField = (renderType) => {
+		let [fieldName, fieldText, fieldType] = getFieldData(renderType);
+
+		return (
+			<div>
+				<label htmlFor={fieldName}>{fieldText}</label>
+				<input
+					type={fieldType}
+					id={fieldName}
+					name={fieldName}
+					onChange={(event) => {
+						propField(renderType, event);
+					}}
+				/>
+			</div>
+		);
 	};
 
 	const handleSubmit = (event) => {
@@ -30,26 +51,33 @@ const CVForm = ({ setFirstName, setLastName }) => {
 
 	return (
 		<form onSubmit={handleSubmit}>
+			{renderField("firstName")}
+			{/*
 			<div>
 				<label htmlFor={"firstNameInput"}>First Name</label>
 				<input
 					type={"text"}
 					id={"firstNameInput"}
 					name={"firstNameInput"}
-					onChange={fnChange}
+					onChange={(event) => {
+						setFirstName(event.target.value);
+						setLocalFirstName(event.target.value);
+					}}
 				/>
 			</div>
-
+			*/}
 			<div>
 				<label htmlFor={"lastNameInput"}>Last Name</label>
 				<input
 					type={"text"}
 					id={"lastNameInput"}
 					name={"lastNameInput"}
-					onChange={lnChange}
+					onChange={(event) => {
+						setLastName(event.target.value);
+						setLocalLastName(event.target.value);
+					}}
 				/>
 			</div>
-
 			<input type={"submit"} value={"Submit"} />
 		</form>
 	);
