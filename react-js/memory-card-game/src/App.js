2b52c77570c6b073bb9df2c87ceeeefa6ec1d858
gamadjg <gamadavid36@gmail.com>
2022-04-08 01:54:17 -0700

    Skeleton setup for memory game

--- react-js/memory-card-game/src/App.js
+++ react-js/memory-card-game/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/App.css";
+import MemoryCards from "./components/memory-cards";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App-header"></div>
+			<div className="App-body">
+				<MemoryCards />
+			</div>
+			<div className="App-footer"></div>
+		</div>
+	);
 }
 
 export default App;
---
+++ react-js/memory-card-game/src/components/memory-cards.js
@@ -0,0 +1,9 @@
+function MemoryCards() {
+	/*
+    
+    */
+
+	return <div>asdflksdjfs;dlfksjd;flds</div>;
+}
+
+export default MemoryCards;
